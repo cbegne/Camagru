8230cd9e956a158bc281c2e935c2b5a97a1fe72f
Celine Begne <Celine@MacBook-Air-de-Celine.local>
2017-04-23 20:58:35 +0200

    Forgot passwd

--- app/home.php
+++ app/home.php
@@ -1,12 +1,11 @@
 <?php
 
 session_start() or die("Failed to resume session\n");
-require 'class/users.php';
+
 
 ?>
 
 <!-- Page d'acceuil si non connecté : possibilité de connexion ou creation de compte -->
-
 
 <html>
 <head>
@@ -20,14 +19,15 @@ require 'class/users.php';
   <form class="" action="#" method="post">
     Identifiant<br /><input type="text" name="login" value=""><br />
     Mot de passe<br /><input type="password" name="passwd" value=""><br />
-    <input type="submit" name="OK" value="OK">
+    <input type="submit" name="submit" value="OK">
   </form>
   <?php
-  if (!empty($_POST['login']) and !empty($_POST['passwd']) and $_POST['submit'] !== "OK") {
+  if (!empty($_POST['login']) and !empty($_POST['passwd']) and $_POST['submit'] == "OK") {
+    require 'class/users.php';
     $login = trim($_POST['login']);
-    $passwd = trim($_POST['passwd']);
-    $db = new Users($login, $passwd, "");
-    $db->connect_user();
+    $passwd = $_POST['passwd'];
+    $db = new Users($login, $passwd, "", "");
+    $db->connectUser();
     if ($db->message)
       echo '<p style="color:red;">' . $db->message . '</p>';
     else {
@@ -36,20 +36,21 @@ require 'class/users.php';
     }
   }
   ?>
-  <!-- Mot de passe oublié ? -->
+  <a href="forgot.php" id="forgot">Mot de passe oublié ?</a>
   <h2>S'inscrire</h2>
   <form class="" action="#" method="post">
     Identifiant<br /><input type="text" name="new_login" value=""><br />
     Mot de passe<br /><input type="password" name="new_passwd" value=""><br />
     Mail<br /><input type="email" name="new_email" value=""><br />
-    <input type="submit" name="new_OK" value="OK">
+    <input type="submit" name="submit_new" value="OK">
     <?php
-    if (!empty($_POST['new_login']) and !empty($_POST['new_passwd']) and !empty($_POST['new_email']) and $_POST['submit'] !== "new_OK") {
+    if (!empty($_POST['new_login']) and !empty($_POST['new_passwd']) and !empty($_POST['new_email']) and $_POST['submit_new'] == "OK") {
+      require 'class/users.php';
       $new_login = trim($_POST['new_login']);
       $new_passwd = $_POST['new_passwd'];
       $new_email = trim($_POST['new_email']);
-      $db = new Users($new_login, $new_passwd, $new_email);
-      $db->add_user();
+      $db = new Users($new_login, $new_passwd, $new_email, "");
+      $db->addUser();
       if ($db->message)
         echo '<p style="color:red;">' . $db->message . '</p>';
     }
