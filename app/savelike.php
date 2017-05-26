9b878bb916ae58f0d7b4bb90ef625e299280d8b7
Celine BEGNE <cbegne@e2r9p3.42.fr>
2017-05-26 16:52:19 +0200

    corr

--- app/savelike.php
+++ app/savelike.php
@@ -1,7 +1,6 @@
 <?php
 
   session_start() or die("Failed to resume session\n");
-  var_dump($_GET);
   $id_pic = $_GET['id_pic'];
   require '../class/likes.class.php';
   $db = new Likes($id_pic, $_SESSION['logged_user']);
